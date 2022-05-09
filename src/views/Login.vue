83fce36946ed6ca4b36c09f41a7b208df71b378d
Spaydevv <spaydevv@gmail.com>
2022-05-09 17:50:25 +0700

    new

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <Form @submit="submit" :validation-schema="schema" v-slot="{ errors }">
+    <Field name="email" />
+    <span>{{ errors.email }}</span>
+    <Field name="password" type="password" rules="required|min:8" />
+    <span>{{ errors.password }}</span>
+    <button>Submit</button>
+  </Form>
+</template>
+<script>
+import { Form, Field } from 'vee-validate';
+export default {
+  components: {
+    Form,
+    Field,
+  },
+  data() {
+    const schema = {
+      email: { required: true },
+      password: { required: true, between: [0, 10] },
+    };
+    return {
+      schema,
+    };
+  },
+};
+</script>
